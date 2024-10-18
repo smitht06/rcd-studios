510284bc52cdee3b81aab24b98f81d09a8d3f82e
RedCyberDragon <51099384+smitht06@users.noreply.github.com>
2024-10-18 14:28:08 -0400

    hello

--- django_project/settings.py
+++ django_project/settings.py
@@ -28,11 +28,11 @@ DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["*"]
 
-csrf_trusted_origins = [
-    "https://*.us.aldryn.io",
-    "https://*.redcyberdragon.net",
-    "http://localhost:8000",
-    "https://*.ondigitalocean.app",
+CSRF_TRUSTED_ORIGINS = [
+    "*.aldryn.io",
+    "*.redcyberdragon.net",
+    "localhost:8000",
+    "*.ondigitalocean.app",
 ]
 
 
